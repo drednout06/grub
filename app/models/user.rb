7b9a07602b3bfefc26733632040a9b0ca0fc9f01
Nurbulat Argumbayev <argnur@gmail.com>
2012-08-21 02:56:39 +0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def name
+  	first_name + " " + last_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Grub::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
