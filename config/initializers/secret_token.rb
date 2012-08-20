89c551ec2ce44f727fa37b50028707c0e758a86a
Nurbulat Argumbayev <argnur@gmail.com>
2012-08-21 00:55:44 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grub::Application.config.secret_token = 'b8071c589b0d5f86ba17bfd543c144280b06d24e81f50ac012d71f521269be2ab75028f6bbf2a279490c21fcf75b52b4ef41c69d40dcd5db0d83c735dda3da60'
