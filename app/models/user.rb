a5a9d3dcbf591e99ed86408af6bb1348420650b3
Nurbulat Argumbayev <argnur@gmail.com>
2012-08-21 03:33:19 +0500

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
@@ -16,4 +17,10 @@ class User < ActiveRecord::Base
   def name
   	first_name + " " + last_name
   end
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
