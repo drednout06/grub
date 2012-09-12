0dd4124b88b88b84a73afd0a44ed66c2f32cb68b
Nurbulat Argumbayev <argnur@gmail.com>
2012-09-12 23:24:46 +0600

    Fix migrations

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Grub
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.i18n.fallbacks = true
   end
 end
