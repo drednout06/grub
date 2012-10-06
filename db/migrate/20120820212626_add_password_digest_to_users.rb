63c4f5692ba9f2393470aa4f705679b913231b41
Nurbulat Argumbayev <argnur@gmail.com>
2012-10-06 18:47:31 +0600

    Fix migrations

--- db/migrate/20120820212626_add_password_digest_to_users.rb
+++ db/migrate/20120820212626_add_password_digest_to_users.rb
@@ -1,5 +1,10 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :password_digest, :string
+  	create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.timestamps
+    end
   end
 end
